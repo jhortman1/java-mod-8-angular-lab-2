9691da3d00da2cdc1bc141d99a86bb79f12fb426
James Hortman <jamesdhortman@gmail.com>
2022-08-16 18:04:26 -0400

    angular lab 2 and 3 complete

--- my-first-angular-app/src/app/app.component.ts
+++ my-first-angular-app/src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+interface athlete{
+  athleteName : string
+  sport : string,
+  active: boolean
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'the best simple Angular app ever!!';
+
+  title = 'Athlete Maker';
+  
+  athleteName!: string; 
+  sport! : string;
+  active!: boolean;
+
+  athletes: athlete []=[];
+
+   onCreateAthlete() {
+        this.athletes.push({
+        athleteName: this.athleteName,
+        sport : this.sport,
+        active: this.active
+      })
+  }
+
+
 }
